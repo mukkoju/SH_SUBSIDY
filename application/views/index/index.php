cda02cd21e37655aa2ba3761bccd2b1e69e79297
satish <sm@viveninfomedia.com>
2015-05-25 17:33:56 +0530

    Steps functionality added

--- application/views/index/index.php
+++ application/views/index/index.php
@@ -89,18 +89,17 @@
                     <a class="subsidy-btn large gvup">YES, I GIVE UP</a>
                 </div>
                 <div class="flw-stps">
-                    <div class="hedng">step1:<b>ASK YOUR MLA/MP</b></div>
+                    <div class="hedng">step2:<b>ASK YOUR MLA/MP</b></div>
                     <div class="dsc">LPG is a subsidized commodity in India and the subsidy amount was a whopping Rs. 40,000 crores during 2013-14. The same money could be used for development purpose. We call upon all LPG consumers who can afford to pay the market price for their LPG supply to be a part of this nation building exercise by giving up LPG subsidy.</div>
                     <a class="subsidy-btn large gvup">SEND A MAIL</a>
                 </div>
                 <div class="flw-stps">
-                    <div class="hedng">step1:<b>SHARE YOUR STORY</b></div>
+                    <div class="hedng">step3:<b>SHARE YOUR STORY</b></div>
                     <textarea></textarea>
                     <a class="subsidy-btn large" style="float: left;">SHARE IT</a>
                     <div class="socl"><i class="icon-facebook" style="color: white !important"></i></div>
                     <div class="socl"><i class="icon-twitter" style="color: white !important"></i></div>
                     <div class="socl"><i class="icon-google" style="color: white !important"></i></div>
-                
                 </div>
             </div>
             <div id="stream">
@@ -143,7 +142,101 @@
             </footer>
         </div>
         <div class="step-model hideElement">
+            <div id="gvup-frm">
+                <div id="pldge" class="stp-frm">
+                    <div class="model-ttl">Step1:  Pledge on Give up LPG subsidy</div>
+                <div class="grid2 grid">
+                    <label>
+                        Name<br>
+                        <input type="text" placeholder="Name" id="pldgr-name">
+                        
+                    </label>
+                </div>
+                <div class="grid2 grid">
+                    <label>
+                        Email<br>
+                        <input type="text" placeholder="Email" id="pldgr-email">
+                    </label>
+                </div>
+                <div class="clear-fix"></div>
+                <div class="grid grid3"><label>
+                        State<br>
+                        <select id="pldgr-state">
+                            <option></option>
+                            <option>Select State</option>
+                        </select>
+                    </label></div>
+                <div class="grid grid3"><label>
+                        Service provider<br>
+                        <select id="pldgr-gas">
+                            <option></option>
+                            <option>Select Service provider</option>
+                            <option>Bharath Gas</option>
+                          <option>HP Gas</option>
+                          <option>Indane Gas</option>
+                        </select>
+                    </label></div>
+                <div class="grid grid3"><label>
+                        Why does you want to give up?<br>
+                        <input type="text" placeholder="Descrption" id="pldgr-dsc">
+                    </label></div>
+                <div class="clear-fix"></div>
+                <div class="pldg-msg">I, <span>Name</span>, pledge to give up, LPG Subsidy and contribute to the nation's success and prosperity of its people. It's my honour to.</div>
+                <div class="subsidy-btn large frm-btn" id="pldge-btn">PLEDGE</div>
+                </div>
+                <div id="reqst" class="stp-frm">
+                     <div class="model-ttl">Step2:Request your MP/MLA for give up subsidy</div>
+                    <div class="grid2 grid">
+                    <label>
+                        State<br>
+                        <select id="rqst-state">
+                            <option>Select State</option>
+                        </select>
+                        
+                    </label>
+                </div>
+                <div class="grid2 grid">
+                    <label>
+                        MLA/MP<br>
+                        <select id="rqst-mp">
+                            <option>Select MP/MLA</option>
+                        </select>
+                    </label>
+                </div>
+                <div class="clear-fix"></div>
+                <div class="grid2 grid">
+                    <label>
+                        Constituency<br>
+                        <select id="rqst-cntncy">
+                            <option>Select Constituency</option>
+                        </select>
+                    </label>
+                </div>
+                <div class="grid2 grid">
+                    <label>
+                        Details shown here<br>
+                        <input type="text" placeholder="@twitter" id="rqst-dtls">
+                    </label>
+                </div>
+                <div class="clear-fix"></div>
+                <div class="pldg-msg">I, <span>Sathish</span>, pledge to give up, LPG Subsidy and contribute to the nation's success and prosperity of its people. It's my honour to.</div>
+                <div class="subsidy-btn large frm-btn" id="rqst-btn">REQUEST</div>
+                </div>
+                <div id="share" class="stp-frm">
+                <div class="model-ttl">Step3: Share your stories</div>
+                <textarea placeholder="Paste your story URL here"></textarea>
+                <div class="clear-fix"></div>
+                <!--<div class="pldg-msg">I, <span>Sathish</span>, pledge to give up, LPG Subsidy and contribute to the nation's success and prosperity of its people. It's my honour to.</div>-->
+                <div style="position: absolute; right: 32%; margin-top: -8px">
+                <div class="socl"><i class="icon-facebook" style="color: white !important"></i></div>
+                <div class="socl"><i class="icon-twitter" style="color: white !important"></i></div>
+                <div class="socl"><i class="icon-google" style="color: white !important"></i></div>
+                </div>
+                <div class="subsidy-btn large frm-btn">Share</div>
+                
+                </div>
             <div class="remove-model" title="Close"><i class="icon-remove"></i></div>
+        </div>
         </div>
     </body>
 </html>
--- public/global/subsidy/subsidy.js
+++ public/global/subsidy/subsidy.js
@@ -44,6 +44,135 @@ $(document).ready(function ($) {
             $('body').find(".mbl-mnu").addClass('actv');
     });
     $('#steps').on('click', '.gvup', function () {
-        alert();
+        $('body').find('.step-model').removeClass('hideElement')
+    });
+    
+    $('body').on('click', '.step-model .remove-model',function(){
+       $(this).parents('.step-model').addClass('hideElement');
+    });
+    
+   $('#pldge').on('keyup', '#pldgr-name', function(){
+       $('#pldge').find('.pldg-msg span').html($(this).val());
+   });
+   
+    $('#pldge').on('click', '#pldge-btn', function () {
+        var elements = $('#gvup-frm #pldge').find("input[type='text'],input[type='password'],input[type='number'],input[type='email'],input[type='radio'],textarea,select");
+        var reg = null;
+        var flag = 0;
+        var error = "Cannot be empty";
+        var element = null;
+        elements.each(function () {
+            element = $(this).attr('id');
+            switch (element) {
+                case 'pldgr-name':
+                    error = 'Name should contain only alphabets, numerals and space';
+                    reg = /^[a-zA-Z0-9 ]+$/;
+                    break;
+                case 'pldgr-email':
+                    error = 'Invalid email';
+                    reg = /^[a-z0-9_\+-]+(\.[a-z0-9_\+-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*\.([a-z]{2,4})$/;
+                    break;
+                case 'pldgr-state':
+                    if ($.trim($(this).find('option:selected').val()) !== '') {
+                        error = 'Please slelect your state';
+                        reg = /^[a-zA-Z0-9 ]+$/;
+                    }
+                    break;
+                case 'pldgr-gas':
+                    if ($.trim($(this).find('option:selected').val()) !== '') {
+                        error = 'Please slelect your service provider';
+                        reg = /^[a-zA-Z0-9 ]+$/;
+                    }
+                    break;
+                case 'pldgr-dsc':
+                    error = null;
+                    reg = null;
+                    break
+            }
+
+            if (reg)
+            {
+                if (!reg.test($(this).val().toLowerCase()))
+                {
+                    flag = 1;
+                    return false;
+                }
+                else
+                {
+                    $(this).parents('label').find('span').remove();
+                }
+            }
+        });
+        if (flag == 1)
+        {
+            $('#' + element).parents('label').append('<span title="' + error + '"><i class="icon-warning-sign"></i></span>');
+            return false;
+        }
+        else
+        {
+            alert('success');
+        var frmWdth = $(this).parents('.stp-frm').outerWidth()+80
+        $(this).parents('.stp-frm').css({'margin-left': -frmWdth});
+        }
+    });
+    
+    
+    $('#reqst').on('click', '#rqst-btn', function () {
+        var elements = $('#gvup-frm #pldge').find("input[type='text'],input[type='password'],input[type='number'],input[type='email'],input[type='radio'],textarea,select");
+        var reg = null;
+        var flag = 0;
+        var error = "Cannot be empty";
+        var element = null;
+        elements.each(function () {
+            element = $(this).attr('id');
+            switch (element) {
+                case 'rqst-state':
+                    if ($.trim($(this).find('option:selected').val()) !== '') {
+                        error = 'Please slelect your state';
+                        reg = /^[a-zA-Z0-9 ]+$/;
+                    }
+                    break;
+                    case 'rqst-mp':
+                    if ($.trim($(this).find('option:selected').val()) !== '') {
+                        error = 'Slelect one MP/MLA';
+                        reg = /^[a-zA-Z0-9 ]+$/;
+                    }
+                    break;
+                    case 'rqst-cntncy':
+                    if ($.trim($(this).find('option:selected').val()) !== '') {
+                        error = 'Select Constituency';
+                        reg = /^[a-zA-Z0-9 ]+$/;
+                    }
+                    break;
+                    case 'rqst-dtls':
+                    error = null;
+                    reg = null;
+                    break
+            }
+
+            if (reg)
+            {
+                if (!reg.test($(this).val().toLowerCase()))
+                {
+                    flag = 1;
+                    return false;
+                }
+                else
+                {
+                    $(this).parents('label').find('span').remove();
+                }
+            }
+        });
+        if (flag == 1)
+        {
+            $('#' + element).parents('label').append('<span title="' + error + '"><i class="icon-warning-sign"></i></span>');
+            return false;
+        }
+        else
+        {
+        alert('success');
+        var frmWdth = $(this).parents('.stp-frm').outerWidth()+80
+        $(this).parents('.stp-frm').css({'margin-left': -frmWdth});
+        }
     });
 });
