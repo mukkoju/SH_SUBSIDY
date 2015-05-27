0f3c4a08d24be080ef7b2e1701fa5e477e175194
satish <sm@viveninfomedia.com>
2015-05-27 11:33:25 +0530

    Model file password changed

--- library/Model.php
+++ library/Model.php
@@ -3,6 +3,6 @@
 class Model {
 
     public function getDBConnection($type, $db) {
-        return (new PDO('mysql:host=localhost;dbname=saddahaq_subsidy' , 'root', 'dambo'));
+        return (new PDO('mysql:host=localhost;dbname=saddahaq_subsidy' , 'root', 'vivenfarms'));
     }
 }
